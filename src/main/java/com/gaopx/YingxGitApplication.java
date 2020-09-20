e171ef8302c9b2759654ced852279660362d333e
gaopx <2100674930@qq.com>
2020-09-21 06:58:02 +0800

    应学Git项目提交 v1.0版

---
+++ src/main/java/com/gaopx/YingxGitApplication.java
@@ -0,0 +1,13 @@
+package com.gaopx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YingxGitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YingxGitApplication.class, args);
+    }
+
+}
