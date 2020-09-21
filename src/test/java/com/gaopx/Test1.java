bf50010bbe41f5042d8cae73a8292015ab528b9e
gao19980518 <71572078+gao19980518@users.noreply.github.com>
2020-09-21 16:16:41 +0800

    Update Test1.java

--- src/test/java/com/gaopx/Test1.java
+++ src/test/java/com/gaopx/Test1.java
@@ -4,5 +4,6 @@ public class Test1 {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("dev intput");
+        System.out.println("远程协助的修改");
     }
 }
