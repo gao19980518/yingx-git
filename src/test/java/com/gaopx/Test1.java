26a58c1f917dd89531d20cd82fa1e9a095a50a85
gaopx <2100674930@qq.com>
2020-09-21 16:52:30 +0800

    应学Git项目提交并推送第6版本 v6.0版

--- src/test/java/com/gaopx/Test1.java
+++ src/test/java/com/gaopx/Test1.java
@@ -6,5 +6,6 @@ public class Test1 {
         System.out.println("dev intput");
         System.out.println("远程协助的修改");
         System.out.println("本地master的提交并推送");
+        System.out.println("再次提交推送");
     }
 }
